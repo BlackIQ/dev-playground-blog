8efb9ebd64f81fc2b7fd64348bb6a68e0ce725bb
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 19:48:36 +0330

    Import models

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,5 @@
 from models.category import CategoryModel
 from models.comment import CommentModel
 from models.post import PostModel
+from models.post_tag import PostTagModel
 from models.tag import TagModel
