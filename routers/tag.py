eff35b06a57f6e6e88f4261407a78e96fe087be2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 15:09:09 +0330

    Base CRUD of tag

---
+++ routers/tag.py
@@ -0,0 +1,94 @@
+# Datetime
+from datetime import datetime, timezone
+
+# FastAPI & SQLAlchemy
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+
+# Dependencies
+from dependencies import get_db
+# Model
+from models import TagModel
+# Schemas
+from schemas import Tag, TagRead
+
+# FastAPI Router
+router = APIRouter(
+    prefix="/tags",
+    tags=["Tag"]
+)
+
+
+@router.get("", response_model=list[TagRead])
+async def all_tags(db: Session = Depends(get_db)):
+    db_tags = db.query(TagModel).all()
+
+    return db_tags
+
+
+@router.get("/{tag_id}", response_model=TagRead)
+async def get_tag(tag_id: int, db: Session = Depends(get_db)):
+    db_tag = db.get(TagModel, tag_id)
+
+    if not db_tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    return db_tag
+
+
+@router.post("", response_model=TagRead, status_code=status.HTTP_201_CREATED)
+async def create_tag(tag: Tag, db: Session = Depends(get_db)):
+    db_tag = TagModel(**tag.model_dump())
+
+    db.add(db_tag)
+
+    db.commit()
+    db.refresh(db_tag)
+
+    return db_tag
+
+
+@router.put("/{tag_id}", response_model=TagRead)
+async def update_tag(tag_id: int, tag: Tag, db: Session = Depends(get_db)):
+    db_tag = db.get(TagModel, tag_id)
+
+    if not db_tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    for key, value in tag.model_dump().items():
+        setattr(db_tag, key, value)
+
+    db.commit()
+    db.refresh(db_tag)
+
+    return db_tag
+
+
+@router.delete("/{tag_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_tag(tag_id: int, db: Session = Depends(get_db)):
+    db_tag = db.get(TagModel, tag_id)
+
+    if not db_tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    db_tag.deleted_at = datetime.now(timezone.utc)
+
+    db.commit()
+    db.refresh(db_tag)
+
+    return None
+
+
+@router.post("/{tag_id}/restore", status_code=status.HTTP_204_NO_CONTENT)
+async def restore_tag(tag_id: int, db: Session = Depends(get_db)):
+    db_tag = db.get(TagModel, tag_id)
+
+    if not db_tag:
+        raise HTTPException(status_code=404, detail="Tag not found")
+
+    db_tag.deleted_at = None
+
+    db.commit()
+    db.refresh(db_tag)
+
+    return None
