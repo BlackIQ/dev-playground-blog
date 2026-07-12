769c5f977d241742fcb8fa30cb1283ce2a05fb62
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 18:42:33 +0330

    Imports in Schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,3 +1,5 @@
 # Schemas
 from schemas.category import Category, CategoryRead
+from schemas.comment import Comment, CommentRead
+from schemas.post import Post, PostRead
 from schemas.tag import Tag, TagRead
