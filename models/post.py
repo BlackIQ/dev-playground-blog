c25a93ec0a7d24c22af3788ef75a7d748003f3ca
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 18:41:18 +0330

    Change some columns in PostModel

--- models/post.py
+++ models/post.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from database.base import Base
 
 
-# Tag Model
+# Post Model
 class PostModel(Base):
     __tablename__ = "posts"
 
@@ -16,6 +16,17 @@ class PostModel(Base):
     title: Mapped[str] = mapped_column(
         nullable=False
     )
+    description: Mapped[str] = mapped_column(
+        nullable=False
+    )
     content: Mapped[str] = mapped_column(
         nullable=False
+    )
+    likes_count: Mapped[int] = mapped_column(
+        nullable=False,
+        default=0
+    )
+    views_count: Mapped[int] = mapped_column(
+        nullable=False,
+        default=0
     )
