a8d4e3d1ba931f902c360db21b7d32fd9a2a12ad
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 14:42:33 +0330

    Base of TagModel

---
+++ models/tag.py
@@ -0,0 +1,21 @@
+# SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
+
+# BaseModel
+from database.base import Base
+
+
+# Tag Model
+class TagModel(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        index=True
+    )
+    name: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    slug: Mapped[str] = mapped_column(
+        nullable=False
+    )
