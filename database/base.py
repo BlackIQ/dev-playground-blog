cfeeb02f54447d754a8f2b411a5a27eb7d99d56d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 14:33:35 +0330

    Create base models for schema and model

---
+++ database/base.py
@@ -0,0 +1,17 @@
+# Pydantic
+from pydantic import BaseModel, ConfigDict
+# SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+# Mixins
+from database.mixins import TimestampMixin, SoftDeleteMixin
+
+
+# Base Class: Model
+class Base(TimestampMixin, SoftDeleteMixin, DeclarativeBase):
+    pass
+
+
+# Base Class: Schema
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
