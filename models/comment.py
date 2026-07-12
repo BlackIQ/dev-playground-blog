c5d39cf2708566619ff4f379b0304ee40bf49fec
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-12 19:47:59 +0330

    Relations in Comment

--- models/comment.py
+++ models/comment.py
@@ -1,5 +1,6 @@
 # SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # BaseModel
 from database.base import Base
@@ -18,4 +19,11 @@ class CommentModel(Base):
     )
     content: Mapped[str] = mapped_column(
         nullable=False
+    )
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id"),
+        nullable=False
+    )
+    post: Mapped["PostModel"] = relationship(
+        back_populates="comments"
     )
