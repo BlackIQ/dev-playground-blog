12d42c8ac0151e0fbe3a84dde1dc765305d04436
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-15 12:20:15 +0330

    Complete post

--- routers/post.py
+++ routers/post.py
@@ -8,92 +8,87 @@ from sqlalchemy.orm import Session
 # Dependencies
 from dependencies import get_db
 # Model
-from models import CategoryModel
+from models import Post
 # Schemas
-from schemas import Category, CategoryRead
+from schemas import PosstCreate, PostUpdate, PostRead
 
 # FastAPI Router
 router = APIRouter(
-    prefix="/categories",
-    tags=["Category"]
+    prefix="/posts",
+    tags=["Post"]
 )
 
 
-@router.get("", response_model=list[CategoryRead])
-async def all_categories(db: Session = Depends(get_db)):
-    db_categorys = db.query(CategoryModel).all()
+@router.get("", response_model=list[PostRead])
+async def all_posts(db: Session = Depends(get_db)):
+    db_posts = db.query(Post).all()
 
-    return db_categorys
-
-
-@router.get("/{category_id}", response_model=CategoryRead)
-async def get_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
-
-    if not db_category:
-        raise HTTPException(status_code=404, detail="Category not found")
-
-    return db_category
+    return db_posts
 
 
-@router.post("", response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
-async def create_category(category: Category, db: Session = Depends(get_db)):
-    db_category = CategoryModel(**category.model_dump())
+@router.get("/{post_id}", response_model=PostRead)
+async def get_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = db.get(Post, post_id)
 
-    db.add(db_category)
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
 
-    db.commit()
-    db.refresh(db_category)
-
-    return db_category
+    return db_post
 
 
-@router.put("/{category_id}", response_model=CategoryRead)
-async def update_category(category_id: int, category: Category, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+@router.post("", response_model=PostRead, status_code=status.HTTP_201_CREATED)
+async def create_post(post: PosstCreate, db: Session = Depends(get_db)):
+    db_post = Post(**post.model_dump())
 
-    if not db_category:
-        raise HTTPException(status_code=404, detail="Category not found")
-
-    for key, value in category.model_dump().items():
-        setattr(db_category, key, value)
+    db.add(db_post)
 
     db.commit()
-    db.refresh(db_category)
+    db.refresh(db_post)
 
-    return db_category
+    return db_post
 
 
-@router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+@router.put("/{post_id}", response_model=PostRead)
+async def update_post(post_id: int, post: PostUpdate, db: Session = Depends(get_db)):
+    db_post = db.get(Post, post_id)
 
-    if not db_category:
-        raise HTTPException(status_code=404, detail="Category not found")
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
 
-    db_category.deleted_at = datetime.now(timezone.utc)
+    for key, value in post.model_dump().items():
+        setattr(db_post, key, value)
 
     db.commit()
-    db.refresh(db_category)
+    db.refresh(db_post)
+
+    return db_post
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = db.get(Post, post_id)
+
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    db_post.deleted_at = datetime.now(timezone.utc)
+
+    db.commit()
+    db.refresh(db_post)
 
     return None
 
 
-@router.post("/{category_id}/restore", status_code=status.HTTP_204_NO_CONTENT)
-async def restore_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+@router.post("/{post_id}/restore", status_code=status.HTTP_204_NO_CONTENT)
+async def restore_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = db.get(Post, post_id)
 
-    if not db_category:
-        raise HTTPException(status_code=404, detail="Category not found")
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
 
-    db_category.deleted_at = None
+    db_post.deleted_at = None
 
     db.commit()
-    db.refresh(db_category)
+    db.refresh(db_post)
 
     return None
-
-
-@router.get("/{category_id}/posts")
-async def category_posts(category_id: int, db: Session = Depends(get_db)):
-    pass
