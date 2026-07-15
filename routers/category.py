6b9c70a941ed555e29a900af9b2a61eb855d87aa
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-15 11:44:26 +0330

    Everything is done in Category module

--- routers/category.py
+++ routers/category.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import Session
 # Dependencies
 from dependencies import get_db
 # Model
-from models import CategoryModel
+from models import Category
 # Schemas
-from schemas import Category, CategoryRead
+from schemas import CategoryCreate, CategoryUpdate, CategoryRead
 
 # FastAPI Router
 router = APIRouter(
@@ -21,14 +21,14 @@ router = APIRouter(
 
 @router.get("", response_model=list[CategoryRead])
 async def all_categories(db: Session = Depends(get_db)):
-    db_categorys = db.query(CategoryModel).all()
+    db_categories = db.query(Category).all()
 
-    return db_categorys
+    return db_categories
 
 
 @router.get("/{category_id}", response_model=CategoryRead)
 async def get_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+    db_category = db.get(Category, category_id)
 
     if not db_category:
         raise HTTPException(status_code=404, detail="Category not found")
@@ -37,8 +37,8 @@ async def get_category(category_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("", response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
-async def create_category(category: Category, db: Session = Depends(get_db)):
-    db_category = CategoryModel(**category.model_dump())
+async def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
+    db_category = Category(**category.model_dump())
 
     db.add(db_category)
 
@@ -49,8 +49,8 @@ async def create_category(category: Category, db: Session = Depends(get_db)):
 
 
 @router.put("/{category_id}", response_model=CategoryRead)
-async def update_category(category_id: int, category: Category, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+async def update_category(category_id: int, category: CategoryUpdate, db: Session = Depends(get_db)):
+    db_category = db.get(Category, category_id)
 
     if not db_category:
         raise HTTPException(status_code=404, detail="Category not found")
@@ -66,7 +66,7 @@ async def update_category(category_id: int, category: Category, db: Session = De
 
 @router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+    db_category = db.get(Category, category_id)
 
     if not db_category:
         raise HTTPException(status_code=404, detail="Category not found")
@@ -81,7 +81,7 @@ async def delete_category(category_id: int, db: Session = Depends(get_db)):
 
 @router.post("/{category_id}/restore", status_code=status.HTTP_204_NO_CONTENT)
 async def restore_category(category_id: int, db: Session = Depends(get_db)):
-    db_category = db.get(CategoryModel, category_id)
+    db_category = db.get(Category, category_id)
 
     if not db_category:
         raise HTTPException(status_code=404, detail="Category not found")
@@ -92,8 +92,3 @@ async def restore_category(category_id: int, db: Session = Depends(get_db)):
     db.refresh(db_category)
 
     return None
-
-
-@router.get("/{category_id}/posts")
-async def category_posts(category_id: int, db: Session = Depends(get_db)):
-    pass
